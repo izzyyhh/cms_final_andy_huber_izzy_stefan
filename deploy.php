9b5d937a9a96feebe0445ea17a88892a7d2e5b2e
Ismail Halili <ihalili.mmt-b2019@fh-salzburg.ac.at>
2021-01-13 10:35:43 +0100

    add deploy.php

---
+++ deploy.php
@@ -0,0 +1,52 @@
+<?php
+namespace Deployer;
+
+require 'recipe/common.php';
+
+// Project name
+set('application', 'andy_huber');
+
+// Project repository
+set('repository', 'git@github.com:izzyyhh/cms_final_andy_huber_izzy_stefan.git');
+
+// [Optional] Allocate tty for git clone. Default value is false.
+set('git_tty', true);
+
+// Shared files/dirs between deploys
+set('shared_files', ['wp-config.php']);
+set('shared_dirs', ['public/wp-content/uploads']);
+
+// Writable dirs by web server
+set('writable_dirs', ['wp-content/uploads']);
+set('allow_anonymous_stats', false);
+// set('writable_mode', 'chown');
+// set('writable_use_sudo', 'true');
+
+// Host
+host('vm-fuchsia.multimediatechnology.at')
+    ->user('admin')
+    ->port(5412)
+    ->set('deploy_path', '~/{{application}}');
+
+// Composer
+set('composer_action', false);
+
+// Tasks
+desc('Deploy your project');
+task('deploy', [
+    'deploy:info',
+    'deploy:prepare',
+    'deploy:lock',
+    'deploy:release',
+    'deploy:update_code',
+    'deploy:shared',
+    'deploy:writable',
+    'deploy:clear_paths',
+    'deploy:symlink',
+    'deploy:unlock',
+    'cleanup',
+    'success'
+]);
+
+// [Optional] If deploy fails automatically unlock.
+after('deploy:failed', 'deploy:unlock');
