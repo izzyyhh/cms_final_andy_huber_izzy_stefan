a0d99eed02d9364b37e1cf2148c5d2c05def3c4d
Ismail Halili <ihalili.mmt-b2019@fh-salzburg.ac.at>
2021-01-14 18:19:03 +0100

    twig work section

--- public/wp-content/themes/andy_huber/index.php
+++ public/wp-content/themes/andy_huber/index.php
@@ -2,5 +2,6 @@
 $context = Timber::context();
 
 $context['posts'] = Timber::get_posts();
+$context['works'] = Timber::get_posts('category_name=work');
 
 Timber::render('views/frontpage.twig', $context);
