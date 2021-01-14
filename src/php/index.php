2deabd07e151f9f70219a2cad172b6b0fe69f64e
stefan33maier@gmail.com <fhs44410>
2021-01-14 18:35:19 +0100

    twig donate section

--- src/php/index.php
+++ src/php/index.php
@@ -3,5 +3,5 @@ $context = Timber::context();
 
 $context['posts'] = Timber::get_posts();
 $context['works'] = Timber::get_posts('category_name=work');
-
+$context['donations'] = Timber::get_posts('category_name=donate');
 Timber::render('views/frontpage.twig', $context);
