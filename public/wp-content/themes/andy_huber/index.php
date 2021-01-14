2be27d25c2c7f821cc96d0eaa75b94c5aceb964d
Ismail Halili <ihalili.mmt-b2019@fh-salzburg.ac.at>
2021-01-14 18:32:49 +0100

    twig envis

--- public/wp-content/themes/andy_huber/index.php
+++ public/wp-content/themes/andy_huber/index.php
@@ -3,5 +3,6 @@ $context = Timber::context();
 
 $context['posts'] = Timber::get_posts();
 $context['works'] = Timber::get_posts('category_name=work');
+$context['environmentals'] = Timber::get_posts('category_name=environmental_issues');
 
 Timber::render('views/frontpage.twig', $context);
