6b0e91c5abcb5197c221260637e9f491068229ac
Ismail Halili <ihalili.mmt-b2019@fh-salzburg.ac.at>
2021-01-15 18:29:50 +0100

    social nav issue

--- public/wp-content/themes/andy_huber/functions.php
+++ public/wp-content/themes/andy_huber/functions.php
@@ -3,8 +3,18 @@ require_once( __DIR__ . '/vendor/autoload.php' );
 
 // enqueue scripts and styles
 function enqueue_scripts_and_styles() {
-    wp_enqueue_style('main-styles', get_template_directory_uri().'/main.3c3575dbeb4111798a88.css?df00bd716376399e12c9', array(), null, false);
-    wp_enqueue_script( 'main-js', get_template_directory_uri().'/main.js?df00bd716376399e12c9', array(), null, false );
+    wp_enqueue_style('main-styles', get_template_directory_uri().'/main.7e66865a281cc7ba2f0a.css?62f90eadc0aad9602ee3', array(), null, false);
+    wp_enqueue_script( 'main-js', get_template_directory_uri().'/main.js?62f90eadc0aad9602ee3', array(), null, false );
+}
+
+//menus
+function register_my_menus() {
+    register_nav_menus(
+      array(
+        'main-navigation' => __( 'Header Menu' ),
+        'footer-navigation' => __( 'Footer Menu' )
+       )
+     );
 }
 
 // enable dynamic title tags
@@ -20,7 +30,17 @@ function cc_mime_types($mimes) {
 add_filter('upload_mimes', 'cc_mime_types');
 add_action( 'after_setup_theme', 'add_title_tag');
 add_action( 'wp_enqueue_scripts', 'enqueue_scripts_and_styles' );
+add_action('init', 'register_my_menus');
 add_theme_support( 'post-thumbnails' );
+add_filter( 'timber/context', 'add_to_context' );
 
 //for timber
+function add_to_context( $context ) {
+    //making menu globallz available
+    $context['menu'] = new \Timber\Menu( 'Header Menu' );
+    $context['socials'] = new \Timber\Menu( 'Footer Socials' );
+    
+    return $context;
+}
+
 $timber = new \Timber\Timber();
--- src/php/functions.php
+++ src/php/functions.php
@@ -7,6 +7,16 @@ function enqueue_scripts_and_styles() {
     wp_enqueue_script( 'main-js', get_template_directory_uri().'/<%=htmlWebpackPlugin.files.chunks.main.entry %>', array(), null, false );
 }
 
+//menus
+function register_my_menus() {
+    register_nav_menus(
+      array(
+        'main-navigation' => __( 'Header Menu' ),
+        'footer-navigation' => __( 'Footer Menu' )
+       )
+     );
+}
+
 // enable dynamic title tags
 function add_title_tag() {
     add_theme_support( 'title-tag' );
@@ -20,7 +30,17 @@ function cc_mime_types($mimes) {
 add_filter('upload_mimes', 'cc_mime_types');
 add_action( 'after_setup_theme', 'add_title_tag');
 add_action( 'wp_enqueue_scripts', 'enqueue_scripts_and_styles' );
+add_action('init', 'register_my_menus');
 add_theme_support( 'post-thumbnails' );
+add_filter( 'timber/context', 'add_to_context' );
 
 //for timber
+function add_to_context( $context ) {
+    //making menu globallz available
+    $context['menu'] = new \Timber\Menu( 'Header Menu' );
+    $context['socials'] = new \Timber\Menu( 'Footer Socials' );
+    
+    return $context;
+}
+
 $timber = new \Timber\Timber();
