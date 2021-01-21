ab7745dbb2a34fb8f6de6f9fcee853d4144bfbed
stefan33maier@gmail.com <fhs44410>
2021-01-21 21:23:00 +0100

    update

--- public/wp-content/themes/andy_huber/page-blog.php
+++ public/wp-content/themes/andy_huber/page-blog.php
@@ -1,6 +1,7 @@
 <?php
 $context = Timber::context();
 
+$context['post'] = Timber::get_post();
 $context['posts'] = Timber::get_posts('category_name=environmental_issues');
 
 Timber::render('views/page-list.twig', $context);
